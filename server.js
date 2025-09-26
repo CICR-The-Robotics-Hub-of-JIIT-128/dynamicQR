e3a355e7c65811efec09e7e4cff100e028c1e3a8
AshutoshThings <ashutoshv9648@gmail.com>
2025-09-27 03:14:10 +0530

    route setup

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const links = require("./links.json");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/r/:id", (req, res) => {
-  const { id } = req.params;
-  console.log(`QR scanned! ID: ${id}, Time: ${new Date().toISOString()}`);
-  res.send(`QR ${id} scanned! Check your terminal.`);
+let path = "";
+
+function getPath(val = 0) {
+  console.log(links[val].linkAddress);
+  path = links[val].linkAddress.toString();
+}
+app.get("/", (req, res) => {
+  console.log("root page requested");
+  res.send("<h1>This Zone is restricted</h1>");
 });
-
+app.get("/cicrRoot", (req, res) => {
+  console.log(`QR scanned!, Time: ${new Date().toISOString()}`);
+  getPath();
+  res.redirect(path);
+});
+app.get("/admin", (req, res) => {
+  console.log(`Admin Login Requested`);
+  res.sendFile("./admin/login.html", { root: __dirname });
+});
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
